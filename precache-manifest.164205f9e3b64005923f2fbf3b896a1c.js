b21b9407fe04e4f3d1da9b09dd03bc69de1c3651
its-sky <smc9919@naver.com>
2022-02-12 17:58:45 +0900

    Updates

---
+++ precache-manifest.164205f9e3b64005923f2fbf3b896a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061b287f6a598e980d23880845c10e5f",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "9375d2a3bfa5ef69f726",
+    "url": "/twitter-clone/static/css/main.389e3af5.chunk.css"
+  },
+  {
+    "revision": "62ccfd152d827c0f6fe3",
+    "url": "/twitter-clone/static/js/2.f9d28bfb.chunk.js"
+  },
+  {
+    "revision": "af57336b2823ba5ffd4833eb4bfcda53",
+    "url": "/twitter-clone/static/js/2.f9d28bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9375d2a3bfa5ef69f726",
+    "url": "/twitter-clone/static/js/main.11f8552c.chunk.js"
+  },
+  {
+    "revision": "516be1713cb27948156e",
+    "url": "/twitter-clone/static/js/runtime-main.64705aa1.js"
+  }
+]);
